8bc6353a7007f91d18a85ab566758d5dad31dda0
GBAE <gabebrett@iinet.net.au>
2022-03-22 17:39:25 +1100

    formik attempt, not yet finished

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
-import AppText from './components/AppText';
-import AppColors from './config/AppColors'; // TODO find better config for this?
-import AppButton from './components/AppButton'; // TODO find better config for this?
+import AppText from './app/components/AppText';
+import AppColors from './app/config/AppColors'; // TODO find better config for this?
+import AppButton from './app/components/AppButton'; // TODO find better config for this?
+import LoginScreen from './app/screens/LoginScreen';
 
 // custom components
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppText>Imagine.</AppText>
-      <AppButton>Enter</AppButton>
-      <StatusBar style="auto" />
+    <View>
+      <LoginScreen/>
     </View>
   );
 }
